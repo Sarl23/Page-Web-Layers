04e7080e0e06987094c0397cd8b29668c3674eb7
jimmyale3102 <jimmyale3102@gmail.com>
2020-11-02 12:16:45 -0500

    Get profile picture

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,44 +1,46 @@
 import React from 'react';
 
-const Resume =(props)=> {
+const Resume = (props) => {
 
-    if(props.data){
-      var education = props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
-      })
+    if (props.data) {
+        var profilePicture = "images/" + props.data.image;
+        var education = props.data.education.map(function (education) {
+            return <div key={education.school}><h3>{education.school}</h3>
+                <p className="info">{education.degree} <span>&bull;</span><em
+                    className="date">{education.graduated}</em></p>
+                <p>{education.description}</p></div>
+        })
+        var work = props.data.work.map(function (work) {
+            return <div key={work.company}><h3>{work.company}</h3>
+                <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+                <p>{work.description}</p>
+            </div>
+        })
     }
 
     return (
-      <section id="resume">
-        <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-          <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+        <section id="resume">
+            <div className="row education">
+                <div className="three columns header-col">
+                    <h1><span>Education</span></h1>
+                </div>
+                <div className="nine columns main-col">
+                    <div className="row item">
+                        <div className="twelve columns">
+                            {education}
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-        <div className="row work">
-          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-          <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
-   </section>
+            <div className="row work">
+                <div className="three columns header-col">
+                    <h1><span>Work</span></h1>
+                </div>
+                <div className="nine columns main-col">
+                    {work}
+                </div>
+            </div>
+        </section>
     );
 }
 
