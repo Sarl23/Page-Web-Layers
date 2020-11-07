6ada0253f6d8e1abb6b655d598baf274441b4cf8
jimmyale3102 <jimmyale3102@gmail.com>
2020-11-07 14:44:13 -0500

    Change carousel 1 picture resource

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,19 +11,19 @@ const Header = (props) => {
       title: 'Vulputate Mollis Ultricies Fermentum Parturient',
       description:
         'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-      image: 'https://i.imgur.com/ZXBtVw7.jpg',
+      image: 'https://raw.githubusercontent.com/Sarl23/Page-Web-Layers/imprv/information/public/images/carousel/carousel_2.jpg?token=AFZOMBGGFKINP4Q7FSXL2XC7U33F2',
     },
     {
       title: 'Tortor Dapibus Commodo Aenean Quam',
       description:
         'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-      image: 'https://i.imgur.com/DCdBXcq.jpg',
+      image: 'https://raw.githubusercontent.com/Sarl23/Page-Web-Layers/imprv/information/public/images/carousel/carousel_1.jpg?token=AFZOMBEOO3532KR5VU6KRZC7U33EG',
     },
     {
       title: 'Phasellus volutpat metus',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-      image: 'https://i.imgur.com/DvmN8Hx.jpg',
+      image: 'https://github.com/Sarl23/Page-Web-Layers/blob/imprv/information/public/images/carousel/carousel_3.jpg?raw=true',
     }
   ];
 
