b63f28cda1f4b8c2a28527681e622fcdad1a8d0e
jimmyale3102 <jimmyale3102@gmail.com>
2020-11-08 17:02:05 -0500

    Change book section strings

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -25,7 +25,7 @@ const Portfolio = (props) => {
     <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My books.</h1>
+          <h1>Mira algunos de nuestros libros</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
             {projects}
           </div>
