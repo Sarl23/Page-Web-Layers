c3919d6d7821e9840ae9aef7700a1333e3e01f01
jimmyale3102 <jimmyale3102@gmail.com>
2020-11-11 13:55:46 -0500

    Update lawyers texts

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -29,29 +29,31 @@ const Resume = (props) => {
                     <div className="row item about-lawyer">
                         <div className="twelve columns">
                             {educationDeibyData}
-                            <lu>Universidad Santo Tomas</lu>
-                            <lu>Magister en Derecho Humanos</lu>
-                            <lu>Doctor en Derecho Público</lu>
-                            <lu>Doctor en Ciencias Jurídicas</lu>
-                            <lu>Doctor en Ciencias de la Educación</lu>
-                            <lu>Doctor en estudios legales con énfasis en Derecho Constitucional</lu>
-                            <lu>Post Doctor en Derecho Humanos</lu>
-                            <br/><br/>
-                            <p>
-                                Con 20 años de experiencia como servidor público, ejerciendo cargos propios de Dirección del programa de Doctorado en Derecho Público de la Universidad Santo Tomas,
-                                Editor de la revista Científica Principia Iuris de la Universidad Santo Tomas,
-                                Docente en el programa de Maestría en Derechos Humanos Universidad Pedagógica y Tecnológica de Colombia
-                                Docente en el programa de Maestría de Derecho Penal y Procesal Penal, Universidad Santo Tomas
-                                Docente en el programa de Especialización en Psicología Jurídica y Forense, Universidad Santo Tomas
-                                Docente asociado en Derecho Penal, Universidad Santo Tomas
-                                Docente asociado en Derecho Penal, Universidad Juan de Castellanos
-                                <br/><br/>
-                                Investigador
-                                <br/>
-                                Escritor
-                                <br/>
-                                Conferencista
-                            </p>
+                            <div className="text-left">
+                                <lu>
+                                    <li>Universidad Santo Tomas</li>
+                                    <li>Magister en Derecho Humanos</li>
+                                    <li>Doctor en Derecho Público</li>
+                                    <li>Doctor en Ciencias Jurídicas</li>
+                                    <li>Doctor en Ciencias de la Educación</li>
+                                    <li>Doctor en estudios legales con énfasis en Derecho Constitucional</li>
+                                    <li>Post Doctor en Derecho Humanos</li>
+                                </lu>
+                                <p>
+                                    Con 20 años de experiencia como servidor público, ejerciendo cargos propios de Dirección del programa de Doctorado en Derecho Público de la Universidad Santo Tomas,
+                                    Editor de la revista Científica Principia Iuris de la Universidad Santo Tomas,
+                                    Docente en el programa de Maestría en Derechos Humanos Universidad Pedagógica y Tecnológica de Colombia
+                                    Docente en el programa de Maestría de Derecho Penal y Procesal Penal, Universidad Santo Tomas
+                                    Docente en el programa de Especialización en Psicología Jurídica y Forense, Universidad Santo Tomas
+                                    Docente asociado en Derecho Penal, Universidad Santo Tomas
+                                    Docente asociado en Derecho Penal, Universidad Juan de Castellanos
+                                </p>
+                                <lu>
+                                    <li>Investigador</li>
+                                    <li>Escritor</li>
+                                    <li>Conferencista</li>
+                                </lu>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -62,13 +64,20 @@ const Resume = (props) => {
                     <div className="row item about-lawyer">
                         <div className="twelve columns">
                             {educationXiomaraData}
-                            <lu>Universidad Santo Tomas</lu>
-                            <lu>Especialista en Derecho Medico – Sanitario Universidad del Rosario</lu>
-                            <lu>Candidata a Magister Universidad del Rosario</lu>
-                            <br/><br/>
-                            <p>
-                                Especialista en Responsabilidad Medico – Sanitaria, con gran experiencia en el litigio, ejerciendo cargos en el sector público, asesora jurídica, cuenta con óptima idoneidad y capacidades en el área respectiva del ejercicio profesional, con amplia experiencia en ámbitos de responsabilidad médica; transparencia y responsabilidad en el asesoramiento, representación, defensa y capacitación de los clientes.
-                            </p>
+                            <div className="text-left">
+                                <lu>
+                                    <li>Universidad Santo Tomas</li>
+                                    <li>Especialista en Derecho Medico – Sanitario Universidad del Rosario</li>
+                                    <li>Candidata a Magister Universidad del Rosario</li>
+                                </lu>
+                                <p>
+                                    Especialista en Responsabilidad Medico – Sanitaria, con gran experiencia en el
+                                    litigio, ejerciendo cargos en el sector público, asesora jurídica, cuenta con óptima
+                                    idoneidad y capacidades en el área respectiva del ejercicio profesional, con amplia
+                                    experiencia en ámbitos de responsabilidad médica; transparencia y responsabilidad
+                                    en el asesoramiento, representación, defensa y capacitación de los clientes.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
